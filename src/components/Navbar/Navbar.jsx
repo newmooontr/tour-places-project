bdfd20626d619179a36d8c7be49c79cd49af66bd
newmoon <newmooon.tr@gmail.com>
2022-10-11 23:51:23 +0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.scss';
+
+const Navbar = () => {
+    return(
+ <div className="navbar">
+      <a href="#">About Us</a>
+      <a href="#">For You</a>
+      <a href="#">Services</a>
+      <a href="#">Blog</a>
+      <a href="#">Vlog</a>
+      <a href="#">Content</a>
+ </div>
+    )
+}
+
+export default Navbar;
+
